63420606a0023f9b7871d5bffeae7f98d0de15ae
bogdanf21 <bogdanfomin21@gmail.com>
2023-02-23 12:31:11 +0200

    Initial commit

---
+++ lab1/lab1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+cam = cv2.VideoCapture("video.mp4")
+
+while True:
+    ret, frame = cam.read()
+    if ret is False:
+        break
+    # Shrink the frame
+    frame = cv2.resize(frame, (480, 360))
+    # Convert the frame to Grayscale
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    trapezoid = np.empty_like(frame)
+    # Add a named window
+    cv2.namedWindow("trapezoid", cv2.WINDOW_NORMAL)
+    #cv2.imshow("trapezoid")
+    cv2.imshow('Original', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
